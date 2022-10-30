210265d4344309713e999ed4ff99f714068bf55b
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2022-10-30 16:27:11 +0200

    refactor

--- app/src/main/java/com/news/db/ArticleDao.kt
+++ app/src/main/java/com/news/db/ArticleDao.kt
@@ -10,7 +10,7 @@ interface ArticleDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun upsert(article: Article): Long
 
-    @Query("SELECT * FROM articles")
+    @Query("SELECT* FROM articles")
     fun getAllArticles(): LiveData<List<Article>>
 
     @Delete
